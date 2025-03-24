8467b62b078715e53e9b8bc2b869a6290a12bcd7
Rafael Apolinario <rafaelma2007@hotmail.com>
2025-03-24 12:00:44 -0300

    comentarios adicionais

---
+++ src/train_model.py
@@ -0,0 +1,4 @@
+# Este arquivo está aqui apenas para exemplificar a estrutura do projeto.
+# Não contém lógica ou implementação real, mas serve como um modelo para treinamento de modelos.
+
+# Aqui poderiam ser definidos os procedimentos de treinamento dos modelos de aprendizado de máquina.
